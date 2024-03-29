14681b0fbbcc78cb285a76cdb9e610ff040f2de5
Bossmaleo1 <bossprogrammaleo@gmail.com>
2024-03-29 21:12:52 +0100

    Initial Commit

---
+++ TikTokTutorial/TikTokTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokTutorialApp.swift
+//  TikTokTutorial
+//
+//  Created by Sidney MALEO on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
